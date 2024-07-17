a3330dab3a2c9f0e6ffdb02a20c3872b62e53252
Avadhoot4757 <avadhootsghewade4757@gmail.com>
2024-07-18 00:43:34 +0530

    undo redo functionality implemented.

--- src/pieces/pieces.cpp
+++ src/pieces/pieces.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 using namespace std;
 
 class Piece
@@ -18,13 +19,14 @@ class PieceTable
     private:
     string* buffer[2];
     vector<Piece> pieces;
+    stack<string> undoStack;
+    stack<string> redoStack;
 
     public:
     PieceTable(string* initial_text, string* add_buffer)
     {
         buffer[0] = initial_text;
         buffer[1] = add_buffer;
-        // cout << "initial text at the time of the contructor is " << (*buffer[0]) << "value of size is " << (*buffer[0]).size()<< endl;
 
         if (!(*initial_text).empty())
             pieces.emplace_back(0, 0, (*initial_text).size());
@@ -39,16 +41,14 @@ class PieceTable
         int count = 0;
         for (Piece p : pieces)
         {
-            cout << " piece :" << count++ << endl;
+            cout << " piece" << count++<<": ";
             int buff = p.buffer;
             int l = p.size;
             int s = p.start_index;
-            // cout << " value of l is " << l << " value of s is " << s << "value of buffer is "<< buff << endl;
             for (int i = 0; i < l; i++)
                 cout << (*buffer[buff])[i + s];
-            cout << endl << "----------------------------------------------------------------------------------------"<< endl;
+            cout<<endl;
         }
-
     }
 
     void push_back(int buff, int start_index, int length)
@@ -56,12 +56,16 @@ class PieceTable
         Piece p = Piece(buff, start_index, length);
         this->insertPiece(p);
     }
+
     void insert(int buff, int index, int start_index, int length)
     {
+        undoStack.push(serializeState());
+        while (!redoStack.empty()) {
+            redoStack.pop();
+        }
+
         Piece p = Piece(buff, start_index, length);
-        int ptr_index = 0;
-        if(index == 0)
-        {
+        if (index == 0) {
             pieces.insert(pieces.begin(), p);
             return;
         }
@@ -69,49 +73,100 @@ class PieceTable
         int i = 0; 
         int j = 0;
 
-        while(j < pieces.size())
-        {
-            // cout << " value of i is " << i << " size of peices[i] " << pieces[j].size << endl;
-            if(index < i + pieces[j].size)
-            {
-                // cout << " the program control was here "<< endl;
-                Piece p2 = Piece(buff, pieces[j].start_index + index - i, pieces[j].size - (index - i));
+        while (j < pieces.size()) {
+            if (index < i + pieces[j].size) {
+                Piece p2 = Piece(pieces[j].buffer, pieces[j].start_index + index - i, pieces[j].size - (index - i));
                 pieces[j].size = index - i;
                 pieces.insert(pieces.begin() + j + 1, p2);
                 pieces.insert(pieces.begin() + j + 1, p);
                 return;
-            }
-            else if(index == i + pieces[j].size)
-            {
+            } else if (index == i + pieces[j].size) {
                 pieces.insert(pieces.begin() + i + 1, p);
                 return; 
             }
             i += pieces[j].size;
             j++; 
-        } 
+        }
+    }
+
+    string serializeState(){
+        string ss;
+        for(const auto& piece : pieces){
+            ss += to_string(piece.buffer) + " ";
+            ss += to_string(piece.start_index) + " ";
+            ss += to_string(piece.size) + ";";
+        }
+        return ss;
+    }
+
+    void deserializeState(string& state){
+        pieces.clear();
+        int start = 0;
+        int end = state.find(';');
+        while (end != string::npos){
+            string piece = state.substr(start, end - start);
+            int first_space = piece.find(' ');
+            int second_space = piece.find(' ', first_space + 1);
+            
+            int buffer = stoi(piece.substr(0, first_space));
+            int start_index = stoi(piece.substr(first_space + 1, second_space - first_space - 1));
+            int size = stoi(piece.substr(second_space + 1));
+            
+            pieces.push_back(Piece(buffer, start_index, size));
+            
+            start = end + 1;
+            end = state.find(';', start);
+        }
+    }
+
+    void undo() {
+        if (!undoStack.empty()) {
+            redoStack.push(serializeState());
+            string state = undoStack.top();
+            undoStack.pop();
+            deserializeState(state);
+        }
+    }
+
+    void redo() {
+        if (!redoStack.empty()) {
+            undoStack.push(serializeState());
+            string state = redoStack.top();
+            redoStack.pop();
+            deserializeState(state);
+        }
     }
 };
 
-int main(){
-    string orignal_buffer= "This_"; 
+int main() {
+    string original_buffer = "This_";
     string addBuffer = "is_the_new_World_";
 
-    PieceTable pt = PieceTable(&orignal_buffer, &addBuffer);
-    cout << " the orignal peice table :"<< endl; 
+    PieceTable pt(&original_buffer, &addBuffer);
+    cout << "Initial piece table:" << endl;
     pt.display();
-    cout << "\n\n\n\n";
-    int size = addBuffer.size();
-    Piece p = Piece(1, 0, size);
-    pt.insertPiece(p);
-    
-    cout << " the peice table 2 is:"<< endl; 
+    cout << "\n";
+
+    int addBufferLength = addBuffer.size();
+    pt.insert(1, original_buffer.size(), 0, addBufferLength);
+    cout << "After inserting the new piece:" << endl;
     pt.display();
-    cout << " the peice table 3s:"<< endl; 
-    cout << "\n\n\n\n";
-    addBuffer.append("Beautiful_");
-    int i = 0;
-    cin >> i; 
-    pt.insert(1, i, size, addBuffer.size() - size);
+    cout << "\n";
+
+    pt.undo();
+    cout << "After undo:" << endl;
     pt.display();
-    return 1;
+    cout << "\n";
+
+    pt.redo();
+    cout << "After redo:" << endl;
+    pt.display();
+    cout << "\n";
+
+    pt.undo();
+    cout << "After another undo:" << endl;
+    pt.display();
+    cout << "\n";
+
+    return 0;
 }
